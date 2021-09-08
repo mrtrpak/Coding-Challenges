6159c94652d3027706765dd5424d10b0b5302669
Michael Trpak <mrtrpak3@gmail.com>
2021-09-08 17:18:11 -0400

    added a second solution

--- Challenges/String Reversal/index.js
+++ Challenges/String Reversal/index.js
@@ -1,12 +1,27 @@
 // Given a string of characters as input, 
 // write a function that returns the characters reversed
 
-const reverseString = string => {
-  console.time("execution time");
+const reverseString1 = string => {
+  console.time("variant 1");
 
-  (string.split("").reverse().join(""));
+  (console.log(string.split("").reverse().join("")));
 
-  console.timeEnd("execution time");
+  console.timeEnd("variant 1");
 };
 
-console.log(reverseString("hello"));
+reverseString1("hello");
+
+
+const reverseString2 = string => {
+  console.time("variant 2");
+  let reversedString = [];
+  
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString.push(string[i]);
+  };
+
+  (console.log(reversedString.join('')));
+  console.timeEnd("variant 2");
+};
+
+reverseString2("flabbergasted");
