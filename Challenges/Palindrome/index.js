3fbef7aa40dc3c223dedf806f28fa313fb7c5f58
Michael Trpak <mrtrpak3@gmail.com>
2021-09-10 15:41:35 -0400

    adding base function to check for palindrome

--- Challenges/Palindrome/index.js
+++ Challenges/Palindrome/index.js
@@ -1,2 +1,10 @@
 // A palindrome is a word or phrase that reads the same backwards as forwards. 
 // Write a function that checks for them.
+
+const checkForPalindrome = string => {
+  const reversedString = string.toLowerCase().split("").reverse().join("");
+
+  console.log(reversedString);
+};
+
+checkForPalindrome("i am A Test SenTence");
