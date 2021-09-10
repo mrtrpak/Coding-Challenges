9f7e7e294bdc84058daa44569ce20aac89842582
Michael Trpak <mrtrpak3@gmail.com>
2021-09-10 16:49:33 -0400

    able to get an integer input and reverse the numbers back into an integer

--- Challenges/Integer Reversal/index.js
+++ Challenges/Integer Reversal/index.js
@@ -1,1 +1,11 @@
-ÿş
+// Given a Number(integer), reverse the order of the digits
+
+const reverseInteger = integer => {
+  const reversedIntegerString = integer.toString().split("").reverse().join("");
+
+  console.log(parseInt(reversedIntegerString));
+
+};
+
+reverseInteger(1234567);
+reverseInteger(2100);
