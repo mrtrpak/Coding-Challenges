2fc1b805a84f3a6c251328f6d3648eefaa8ceff9
Michael Trpak <mrtrpak3@gmail.com>
2021-09-10 16:16:52 -0400

    able to determine if submitted string is a palindrome or not with function

--- Challenges/Palindrome/index.js
+++ Challenges/Palindrome/index.js
@@ -2,9 +2,18 @@
 // Write a function that checks for them.
 
 const checkForPalindrome = string => {
-  const reversedString = string.toLowerCase().split("").reverse().join("");
+  const reversedStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split("").reverse().join("").split(" ").join("");
 
-  console.log(reversedString);
+  const initialStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split(" ").join("");
+
+  if (reversedStringToCheck === initialStringToCheck) {
+    return console.log(`${string} is a palindrome! AWESOME :)`);
+  } else {
+    return console.log(`${string} is NOT a palindrome, bummer`);
+  }
 };
 
 checkForPalindrome("i am A Test SenTence");
+checkForPalindrome("Holloh");
+checkForPalindrome("Was it a car or a cat I saw");
+checkForPalindrome("was it a car or a cat I saw?");
