0c743afae6500ed30d27750899de252c15959b7f
Michael Trpak <mrtrpak3@gmail.com>
2021-09-10 16:23:01 -0400

    adding second solution with ternary instead of if statement

--- Challenges/Palindrome/index.js
+++ Challenges/Palindrome/index.js
@@ -1,7 +1,7 @@
 // A palindrome is a word or phrase that reads the same backwards as forwards. 
 // Write a function that checks for them.
 
-const checkForPalindrome = string => {
+const checkForPalindrome1 = string => {
   const reversedStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split("").reverse().join("").split(" ").join("");
 
   const initialStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split(" ").join("");
@@ -10,10 +10,25 @@ const checkForPalindrome = string => {
     return console.log(`${string} is a palindrome! AWESOME :)`);
   } else {
     return console.log(`${string} is NOT a palindrome, bummer`);
-  }
+  };
+
 };
 
-checkForPalindrome("i am A Test SenTence");
-checkForPalindrome("Holloh");
-checkForPalindrome("Was it a car or a cat I saw");
-checkForPalindrome("was it a car or a cat I saw?");
+checkForPalindrome1("i am A Test SenTence");
+checkForPalindrome1("Holloh");
+checkForPalindrome1("Was it a car or a cat I saw");
+checkForPalindrome1("was it a car or a cat I saw?");
+
+
+const checkForPalindrome2 = string => {
+  const reversedStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split("").reverse().join("").split(" ").join("");
+
+  const initialStringToCheck = string.replace(/[!?,:;'"]/g, "").toLowerCase().split(" ").join("");
+
+  reversedStringToCheck === initialStringToCheck ? 
+    console.log(`${string} is a palindrome! AWESOME :)`) :
+    console.log(`${string} is NOT a palindrome, bummer`)
+};
+
+checkForPalindrome2("was it a car or a cat I saw?");
+checkForPalindrome2("neEettTeeen");
