90d0c774467f6c1f05d74af20398596c73e4f9d8
Michael Trpak <mrtrpak3@gmail.com>
2021-09-15 17:22:38 -0400

    able to display text file data in html

---
+++ Challenges/Read Text File/index.js
@@ -0,0 +1,25 @@
+const readTxtFile = () => {
+  document.getElementById("read-button").addEventListener("click", () => {
+    if (document.querySelector("#file-input").files.length == 0) {
+      alert('Error : No file selected');
+      return;
+    };
+
+    let file = document.getElementById("file-input").files[0];
+    let reader = new FileReader();
+
+    reader.addEventListener("load", e => {
+      let text = e.target.result;
+
+      document.getElementById("file-content").textContent = text;
+    });
+
+    reader.addEventListener('error', function() {
+        alert('Error : Failed to read file');
+    });
+
+    reader.readAsText(file);
+  });
+};
+
+readTxtFile();
