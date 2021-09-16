0f523ade41e0d052a46ecc5cbca75081d57d20b6
Michael Trpak <mrtrpak3@gmail.com>
2021-09-16 12:58:37 -0400

    able to create a text file using fs

--- Challenges/Create Text File/index.js
+++ Challenges/Create Text File/index.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 
 let data = "You successfully created a text file.";
 
-fs.writeFile('index.txt', data, err => {
+// write file will create a new file with the specified data in it. If file
+// already exists it will replace the file.
+fs.writeFile("index.txt", data, err => {
+  if (err) throw err;
+});
+
+// append file will add content to specified file. If no file exists it will create it.
+fs.appendFile("index.txt", " I added to file!", err => {
   if (err) throw err;
 });
