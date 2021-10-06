fc5a3392190a8a1f8da63f09cc4c1e1d914acc0f
Michael Trpak <mrtrpak3@gmail.com>
2021-10-06 14:25:43 -0400

    adding solution for fibonacci

--- Challenges/Fibonacci/index.js
+++ Challenges/Fibonacci/index.js
@@ -1,2 +1,10 @@
 // implement a function that returns a fibonacci number
 // at a given index.
+
+// fibonacci: sum of the previous 2 numbers
+
+const fibonacci1 = number => {
+  number < 3 ? 1 : fibonacci(number - 1) + fibonacci(number - 2);
+};
+
+fibonacci1(15);
