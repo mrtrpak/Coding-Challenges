f588d73dae91f9f48d08eb196ab15f8f9e946d69
Michael Trpak <mrtrpak3@gmail.com>
2021-10-13 11:00:53 -0400

    adding second solution

--- Challenges/Staircase/index.js
+++ Challenges/Staircase/index.js
@@ -1,7 +1,7 @@
 // For a given number of steps print out a staircase
 // using hashes and spaces
 
-const staircase = number => {
+const staircase1 = number => {
   let stairs = "";
 
   for (let row = 0; row < number; row ++) {
@@ -14,4 +14,14 @@ const staircase = number => {
   };
 
   return console.log(stairs);
+};
+
+staircase1(25);
+
+const staircase2 = (number, row = 0, stair = "", stairs = "") => {
+  if (row === number) return console.log(stairs);
+  if (stair.length === number) 
+    return staircase2(number, row + 1, "", stairs + stair + "\n");
+
+  return staircase2(number, row, stair + (stair.length <= row ? "#" : " "), stairs);
 };
