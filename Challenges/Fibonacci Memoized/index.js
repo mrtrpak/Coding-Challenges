445bd3d64018ea555699e4c4dcd54f95d7c4757b
Michael Trpak <mrtrpak3@gmail.com>
2021-10-13 10:21:30 -0400

    adding first solution to problem

--- Challenges/Fibonacci Memoized/index.js
+++ Challenges/Fibonacci Memoized/index.js
@@ -1,2 +1,15 @@
 // Implement a performant recursive function for fibonacci
 
+const fibonacci1 = (element, cache = []) => {
+  if (cache[element])
+    return console.log(cache[element]);
+  else {
+    if (element < 3)
+      return console.log(1);
+    else 
+      cache[element] = fibonacci1(element - 1, cache)
+      + fibonacci1(element - 2, cache);
+  }
+
+  return console.log(cache[element]);
+};
