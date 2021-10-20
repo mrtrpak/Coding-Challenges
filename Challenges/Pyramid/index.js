e7177616d01ea3fd54b61d3bb6b7c29d9e8296c1
Michael Trpak <mrtrpak3@gmail.com>
2021-10-20 15:57:28 -0400

    adding solution for challenge

--- Challenges/Pyramid/index.js
+++ Challenges/Pyramid/index.js
@@ -1,3 +1,16 @@
 // For a given number of levels print out a pyramid 
 // using hashes and spaces
 
+const pyramid = number => {
+  let levels = "";
+  const midpoint = Math.floor((2 * number - 1) / 2);
+
+  for (let row = 0; row < number; row++) {
+    let level = "";
+    for (let column = 0; column < 2 * number - 1; column++)
+      level += midpoint - row <= column && column <= midpoint + row ? "#" : " ";
+    levels += level + "\n";
+  }
+
+  return levels;
+};
